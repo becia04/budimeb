2d0c1d1eb4f65885a3f60debd06ffa4752ad761e
Beata Bethke <beatabet99@gmail.com>
2024-07-12 11:54:53 +0200

    Dodaj pliki projektów.

---
+++ budimeb/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace budimeb.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Project> Projects { get; set; }
+    }
+}
